b9b338c6f9e7571cdcdf97898d9c55d45642e211
Abhishek Sirohi <abhiiit0306@gmail.com>
2020-11-08 19:43:06 +0530

    Updates

---
+++ precache-manifest.e24a6bc2a78da55673a9390245e2d615.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be48a71b76a10bf9624ccc612dd0d672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7852a4cc0b3c6019b85",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bffbac647345dc5de6c4",
+    "url": "/static/css/main.7d754f00.chunk.css"
+  },
+  {
+    "revision": "c7852a4cc0b3c6019b85",
+    "url": "/static/js/2.db311ddb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.db311ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffbac647345dc5de6c4",
+    "url": "/static/js/main.cdf32c95.chunk.js"
+  },
+  {
+    "revision": "6a8f807b53dc9dd0dd7f",
+    "url": "/static/js/runtime-main.4320b991.js"
+  },
+  {
+    "revision": "053b98eada99968f4289f33c5faf3e69",
+    "url": "/static/media/icdsa.053b98ea.png"
+  },
+  {
+    "revision": "17edeef1f81679a1fb425ed318713f0d",
+    "url": "/static/media/pic.17edeef1.jpeg"
+  },
+  {
+    "revision": "79e88a656e6721e8eab872107c859ef6",
+    "url": "/static/media/safepe.79e88a65.png"
+  }
+]);
